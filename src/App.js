015e8d078b0213ab97b9d451682e1bc7b4d50314
rahul darla <darla.rahul@gmail.com>
2023-05-01 23:55:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
+
+// import { extend, useFrame, useThree } from "@react-three/fiber";
+// import React, { useRef } from "react";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// extend({ OrbitControls });
+
+// const Experience = () => {
+//   const { camera, gl } = useThree();
+//   const cubeRef = useRef();
+//   const groupRef = useRef();
+
+//   useFrame((state, delta) => {
+//     cubeRef.current.rotation.y += delta;
+//     groupRef.current.rotation.y += delta;
+//   });
+
+//   return (
+//     <>
+//       <orbitControls args={[camera, gl.domElement]} />
+//       <directionalLight />
+//       <ambientLight />
+//       <group ref={groupRef}>
+//         <mesh position-x={-2}>
+//           <sphereGeometry />
+//           <meshStandardMaterial color="orange" wireframe />
+//         </mesh>
+
+//         <mesh
+//           ref={cubeRef}
+//           rotation-y={Math.PI * 0.25}
+//           position-x={2}
+//           scale={1.5}
+//         >
+//           <boxGeometry scale={1.5} />
+//           <meshStandardMaterial color="mediumPurple" />
+//         </mesh>
+//       </group>
+
+//       <mesh position-y={-1} scale={10} rotation-x={-Math.PI * 0.5}>
+//         <planeGeometry />
+//         <meshStandardMaterial color="greenyellow" />
+//       </mesh>
+//     </>
+//   );
+//   // npm install @react-three/drei@9.32
+// };
+
+// export default Experience;
---
+++ src/Experience.js
@@ -0,0 +1,10 @@
+import { OrbitControls } from "@react-three/drei";
+
+export default function Experience() {
+  return (
+    <>
+      <color args={["#695b5b"]} attach="background" />
+    </>
+  );
+}
+// https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf
