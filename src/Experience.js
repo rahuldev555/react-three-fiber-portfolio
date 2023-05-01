51de3a50a13d4c422680e1b9895b75c250c6af79
rahul darla <darla.rahul@gmail.com>
2023-05-02 02:52:30 +0530

    r3f portfolio done

--- src/Experience.js
+++ src/Experience.js
@@ -1,10 +1,63 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  useGLTF,
+  Float,
+  PresentationControls,
+  ContactShadows,
+  Html,
+  Text,
+} from "@react-three/drei";
 
 export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
   return (
     <>
+      <Environment preset="city" />
+
       <color args={["#695b5b"]} attach="background" />
+
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://rahuldarla-portfolio.vercel.app/" />
+            </Html>
+          </primitive>
+          <Text
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign="center"
+          >
+            Rahul Darla
+          </Text>
+        </Float>
+      </PresentationControls>
+
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={15} blur={2.4} />
     </>
   );
 }
-// https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf
